1140e0374aea20b44ab84ac19f5742fabfd2589c
lekolawole <lekolawole@gmail.com>
2022-10-22 16:09:50 -0400

    Adding Discover page

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+// Fetching data from RapidApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '1035f68cccmshb5a3ad401bc169cp10ca28jsnf2e023137d99');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
