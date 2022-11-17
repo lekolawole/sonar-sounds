4184708c8adf08f1d97796d1658564d372beb9fb
lekolawole <lekolawole@gmail.com>
2022-11-17 13:05:45 +0000

    Adding endpoints & api calls

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -13,9 +13,17 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    getSongsByCountry: builder.query({ query: (conutryCode) => `/charts/country?country_code=${conutryCode}` }),
   }),
 });
 
 export const {
   useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
 } = shazamCoreApi;
